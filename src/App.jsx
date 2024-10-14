d57d0121d89f71a7e6ef359a4d453506193e4a35
morningstar0000 <miraclemorningstar9@gmail.com>
2024-10-14 11:05:24 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+
+function App() {
+ 
+
+  return (
+    <main>
+       <Navbar/>
+       <CartContainer/>
+    </main>
+  )
+}
+
+export default App
